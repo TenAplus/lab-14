c538dfc4b22684a4fb12cb7bfc76c837871024e4
TenAplus <pongthitheethawat@gmail.com>
2025-01-28 13:49:23 +0700

    Update lab14_1.cpp

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -5,6 +5,28 @@ template <typename T>
 void insertionSort(T d[],int N){
 
 }
+  int i,  j;
+	T key ;
+    for (i = 1; i < N; i++) {
+		cout <<"Pass "<< i << ":";
+        key = d[i];
+        j = i - 1;
+
+        while (j >= 0 && d[j] < key) {
+            d[j + 1] = d[j];
+            j = j - 1;
+        }
+        d[j + 1] = key;
+		for(int k=0;k<N;k++){
+			cout << d[k]<<" ";
+		}
+		cout << "\n";
+    }
+	
+}
+
+}
+
 
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
@@ -17,3 +39,5 @@ int main(){
 	cout << "\nSorted Array:";
 	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
 }
+
+  
